408a661d34cf11f452ba64e36e4c57c0d73ba1a8
S0umyajit <soumyajit2k18@gmail.com>
2026-04-28 01:39:15 +0530

    Has A RelationShip Aggregation

---
+++ src/main/java/com/example/hasAReationAssociation/MyTest.java
@@ -0,0 +1,28 @@
+package com.example.hasAReationAssociation;
+class Address{
+    String city="Shanksahar";
+    String state="WestBengal";
+
+    public void printAddress(){
+        System.out.println(city+"( "+state+" )");
+    }
+}
+class Student{
+    int age;
+    String name;
+    Address address=new Address();
+    public void printDetails(){
+        System.out.println(age+" "+name);
+        address.printAddress();
+    }
+}
+
+public class MyTest {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        s1.name="Soumyajit";
+        s1.age=26;
+        s1.printDetails();
+
+    }
+}
