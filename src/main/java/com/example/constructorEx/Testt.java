c4e65c0304c48f1a76cdee9730470f031e159b79
S0umyajit <soumyajit2k18@gmail.com>
2026-04-28 00:51:45 +0530

    My test

---
+++ src/main/java/com/example/constructorEx/Testt.java
@@ -0,0 +1,26 @@
+package com.example.constructorEx;
+
+public class Testt {
+
+    int age;
+    String name;
+
+    Testt(){
+        this(0,null); // calling the parameterized constructor; constructor chain
+    }
+
+    Testt(int age,String name){
+        this.age=age;
+        this.name=name;
+    }
+
+    public void printM(){
+        System.out.println(age+" "+name);
+    }
+
+    public static void main(String[] args) {
+        Testt t=new Testt();
+        t.printM();
+
+    }
+}
