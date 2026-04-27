d2a89c485addaf8990c0cc67924275cd90736712
S0umyajit <soumyajit2k18@gmail.com>
2026-04-27 23:19:40 +0530

    Static Nested Class

--- src/main/java/com/example/classAndObj/Prac.java
+++ src/main/java/com/example/classAndObj/Prac.java
@@ -1,9 +1,15 @@
 package com.example.classAndObj;
 class OuterClass{
 
+    public void callMe(){
+        System.out.println("Hello from Outer");
+    }
+
     static class InnerClass{
         public void printRes(){
             System.out.println("hello from Inner class");
+            OuterClass outerClass=new OuterClass();
+            outerClass.callMe();
         }
     }
 }
