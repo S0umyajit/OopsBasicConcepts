b863d9ef9657a963b62bf2f0095791e7ed5fb96d
S0umyajit <soumyajit2k18@gmail.com>
2026-04-27 23:38:02 +0530

    Local Inner class

---
+++ src/main/java/com/example/classAndObj/OuterClassTT.java
@@ -0,0 +1,23 @@
+package com.example.classAndObj;
+
+/**
+ * This is an example for local Inner class
+ */
+public class OuterClassTT {
+
+    public void myMeth(){
+        class InnerClass{
+
+            public void innerMeth(){
+                System.out.println("Hi from inner meth");
+            }
+        }
+        InnerClass innerClass=new InnerClass();
+       innerClass.innerMeth();
+    }
+
+    public static void main(String[] args) {
+        OuterClassTT outerClassTT=new OuterClassTT();
+        outerClassTT.myMeth();
+    }
+}
